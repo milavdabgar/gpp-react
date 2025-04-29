81bf555d0a16eea724e3a6332ace4c039ce88461
Milav Dabgar <milav.dabgar@gmail.com>
2025-04-29 07:55:17 +0530

    Convoy Year to Convo Year

--- src/pages/admin/Student.tsx
+++ src/pages/admin/Student.tsx
@@ -547,7 +547,7 @@ const Student = () => {
                 Admission Year
               </th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Convoy Year
+                Convo Year
               </th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Status
